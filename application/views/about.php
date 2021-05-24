1839c28ef7bb3d86b68c316295bd512dd4b06570
pangestuf <61309801+pangestuf@users.noreply.github.com>
2021-05-24 23:04:17 +0700

    upload code

---
+++ application/views/about.php
@@ -0,0 +1,2 @@
+<h1>About</h1>
+<p>Halo nama saya Farras, saat ini saya sedang belajar di ITS jurusan Teknik Informatika</p>
---
+++ application/views/contact.php
@@ -0,0 +1,4 @@
+<h1>Contact</h1>
+<p>Find Me at</p>
+<p>Facebook: Farras Pangestu</p>
+<p>Github: pangestuf</p>
